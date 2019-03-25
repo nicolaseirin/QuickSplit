3a52a9257147a31a06944f6151f760f6892ced35
Eusebio <eusebiodm98@gmail.com>
2019-03-25 20:56:11 -0300

    Remade backend project

--- src/BackEnd/QuickSplit/QuickSplit.WebApi/Program.cs
+++ src/BackEnd/QuickSplit/QuickSplit.WebApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ObligatorioArqBackEnd.WebApi
+namespace QuickSplit.WebApi
 {
     public class Program
     {
