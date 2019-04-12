2716e13c366a61a6a8f7783db5a38313c725335b
Ana Clara <aniirodferr@gmail.com>
2019-04-12 14:22:19 -0300

    Se crea la entidad Group

---
+++ src/BackEnd/QuickSplit/QuickSplit.Domain/Group.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSplit.Domain
+{
+    public class Group
+    {
+        private string name;
+        private string admin;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                ValidateNotNullOrEmpty(value, "Name");
+                name = value;
+            }
+        }
+
+        public string Admin
+        {
+            get => admin;
+            set
+            {
+                ValidateNotNullOrEmpty(value, "Administrator");
+                name = value;
+            }
+        }
+
+        private void ValidateNotNullOrEmpty(string value, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new DomainException($"{propertyName} is required");
+        }
+    }
+}
