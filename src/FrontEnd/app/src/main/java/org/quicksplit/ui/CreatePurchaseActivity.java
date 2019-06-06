16f3e42f4c938f8ab64898926831ee7aa5df6a0b
Unknown <aniirodferr@gmail.com>
2019-06-06 18:36:20 -0300

    Implemented current location

--- src/FrontEnd/app/src/main/java/org/quicksplit/ui/CreatePurchaseActivity.java
+++ src/FrontEnd/app/src/main/java/org/quicksplit/ui/CreatePurchaseActivity.java
@@ -1,9 +1,17 @@
 package org.quicksplit.ui;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -19,6 +27,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import org.quicksplit.R;
 import org.quicksplit.ServiceGenerator;
@@ -100,9 +109,6 @@ public class CreatePurchaseActivity extends AppCompatActivity implements  View.O
 
         getData();
 
-        if (isServicesOk()){
-            initMap();
-        }
     }
 
     private void getData() {
@@ -214,30 +220,5 @@ public class CreatePurchaseActivity extends AppCompatActivity implements  View.O
 
         purchase.setParticipants(participantsString);
         purchase.setPurchaser(tokenManager.getUserIdFromToken());
-    }
-
-    private boolean isServicesOk(){
-        Log.d(TAG, "isServicesOk: checking google map services");
-
-        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(CreatePurchaseActivity.this);
-        if(available == ConnectionResult.SUCCESS){
-            //everything is fine and the user can make map requests
-            Log.d(TAG, "isServicesOK: Google Play Services is working");
-            return true;
-        }
-        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
-            //an error occured but we can resolve it
-            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(CreatePurchaseActivity.this, available, ERROR_DIALOG_REQUEST);
-            dialog.show();
-        }
-        else{
-            Toast.makeText(this, "You can not make map requests", Toast.LENGTH_SHORT/*.show()*/);
-        }
-        return false;
-    }
-
-    private void initMap(){
-
     }
 }
--- src/FrontEnd/app/src/main/java/org/quicksplit/ui/MapActivity.java
+++ src/FrontEnd/app/src/main/java/org/quicksplit/ui/MapActivity.java
@@ -2,6 +2,8 @@ package org.quicksplit.ui;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -10,27 +12,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.quicksplit.R;
 
-public class MapActivity extends AppCompatActivity implements  OnMapReadyCallback{
+import java.util.Map;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MapActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private static final  int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
 
     private Boolean mLocationPermissionGranted = false;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this,"Map is ready", Toast.LENGTH_SHORT);
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT);
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
+
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
     @Override
@@ -39,6 +63,37 @@ public class MapActivity extends AppCompatActivity implements  OnMapReadyCallbac
         setContentView(R.layout.activity_map);
 
         getLocationPermission();
+    }
+
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDevicieLocation: getting the devices current location");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            if(mLocationPermissionGranted){
+                Task<Location> location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM );
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(MapActivity.this, "unable to get current location", Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+            }
+        }catch(SecurityException e){
+            Log.d(TAG, "getDeviceLocation: SecurityException " + e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latlng, float zoom){
+        Log.d(TAG, "moveCamera: moving the camera to: lat" + latlng.latitude + ", lng: " + latlng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
+
     }
 
     private void initMap(){
@@ -59,10 +114,13 @@ public class MapActivity extends AppCompatActivity implements  OnMapReadyCallbac
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 mLocationPermissionGranted = true;
+                initMap();
             }else{
                 ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
             }
-        }
+        }else{
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+    }
     }
 
     @Override
