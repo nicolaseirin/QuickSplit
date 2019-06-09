dc7da5840391484dbd1f2598f10ee7514cd368a1
Nicolás Eirin <nicoeirin@gmail.com>
2019-06-09 19:24:30 -0300

    Implemented file chooser

--- src/FrontEnd/app/src/main/java/org/quicksplit/ui/CreatePurchaseActivity.java
+++ src/FrontEnd/app/src/main/java/org/quicksplit/ui/CreatePurchaseActivity.java
@@ -1,16 +1,28 @@
 package org.quicksplit.ui;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -24,15 +36,43 @@ import org.quicksplit.models.User;
 import org.quicksplit.service.CurrencyClient;
 import org.quicksplit.service.GroupClient;
 import org.quicksplit.service.PurchaseClient;
+import org.quicksplit.service.UserClient;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CreatePurchaseActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //TEST
+    private static int PICK_IMAGE_CAMERA = 1;
+    private static int PICK_IMAGE_GALLERY = 2;
+    private ImageView imageview;
+    private Button btnSelectImage;
+    private Bitmap bitmap;
+    private File destination = null;
+    private InputStream inputStreamImg;
+    private String imgPath = null;
+    //FIN TEST
+
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    private String filePath;
 
     private List<Group> groups;
     private ArrayAdapter<Group> groupArrayAdapter;
@@ -59,6 +99,8 @@ public class CreatePurchaseActivity extends AppCompatActivity implements View.On
     private RecyclerView mRecyclerViewGroupMembers;
     private GroupFriendsAdapter mRecycleViewGroupFriendsAdapter;
     private RecyclerView.LayoutManager mRecyclerViewManager;
+
+    private Button mButtonUploadImage;
     private Button mButtonCreatePurchase;
 
     @Override
@@ -100,6 +142,15 @@ public class CreatePurchaseActivity extends AppCompatActivity implements View.On
 
         mTextInputLayoutTxtCost = findViewById(R.id.lblError_txtCost);
         mEditTextCost = findViewById(R.id.txt_cost);
+
+        mButtonUploadImage = findViewById(R.id.btn_uploadImage);
+        mButtonUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //getPurchaseImage();
+                selectImage();
+            }
+        });
 
         mButtonCreatePurchase = findViewById(R.id.btn_createPurchase);
         mButtonCreatePurchase.setOnClickListener(this);
@@ -228,6 +279,53 @@ public class CreatePurchaseActivity extends AppCompatActivity implements View.On
         });
     }
 
+    private void getPurchaseImage() {
+        openGallery();
+    }
+
+    private void openGallery() {
+        ActivityCompat.requestPermissions(CreatePurchaseActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+    }
+
+    /*
+    @Override
+    protected void onActivityResult(int recuestCode, int resultCode, Intent data) {
+        super.onActivityResult(recuestCode, resultCode, data);
+
+        if (recuestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePath = {MediaStore.Images.Media.DATA};
+            Cursor c = getContentResolver().query(selectedImage, filePath,
+                    null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String FilePathStr = c.getString(columnIndex);
+            c.close();
+
+            this.filePath = FilePathStr;
+        }
+    }*/
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(i, RESULT_LOAD_IMAGE);
+                } else {
+                    Toast.makeText(CreatePurchaseActivity.this, "Permission denied to read your External storage.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
     @Override
     public void onClick(View v) {
         createPurchase();
@@ -266,6 +364,7 @@ public class CreatePurchaseActivity extends AppCompatActivity implements View.On
             @Override
             public void onResponse(Call<Purchase> call, Response<Purchase> response) {
                 if (response.isSuccessful()) {
+                    uploadImageToServer(response.body());
                     loading.dismiss();
                 } else {
                     loading.dismiss();
@@ -281,18 +380,48 @@ public class CreatePurchaseActivity extends AppCompatActivity implements View.On
         });
     }
 
+    private void uploadImageToServer(Purchase purchase) {
+        if(imgPath != null) {
+            TokenManager tokenManager = new TokenManager(this);
+
+            PurchaseClient client = ServiceGenerator.createService(PurchaseClient.class, tokenManager.getToken());
+
+            destination = new File(imgPath);
+
+            RequestBody fileReqBody = RequestBody.create(MediaType.parse("image/" + filePath.substring(filePath.lastIndexOf(".") + 1)), destination);
+            MultipartBody.Part part = MultipartBody.Part.createFormData("image", destination.getName(), fileReqBody);
+
+            Call call = client.addPurchaseImage(tokenManager.getUserIdFromToken(), part);
+            call.enqueue(new Callback() {
+                @Override
+                public void onResponse(Call call, Response response) {
+                    if (response.isSuccessful()) {
+                        Toast.makeText(CreatePurchaseActivity.this, "La imagen se actualizó correctamente.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        System.out.println("Error al actualizar la imagen.");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call call, Throwable t) {
+                    Toast.makeText(CreatePurchaseActivity.this, "Error en la conexión al actualizar la imagen.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
     private void showFormErrors() {
-        if(!(mEditTextPurchaseName.getText().toString().trim().length() > 0))
+        if (!(mEditTextPurchaseName.getText().toString().trim().length() > 0))
             mTextInputLayoutPurchaseName.setError("El nombre es requerído.");
         else
             mTextInputLayoutPurchaseName.setError("");
 
-        if(!(mEditTextCost.getText().toString().trim().length() > 0))
+        if (!(mEditTextCost.getText().toString().trim().length() > 0))
             mTextInputLayoutTxtCost.setError("El costo es requerído.");
         else
             mTextInputLayoutTxtCost.setError("");
 
-        if(!(participants.size() > 0))
+        if (!(participants.size() > 0))
             mTextInputLayoutGroupMembers.setError("Por lo menos debe seleccionar un miembro para la compra.");
         else
             mTextInputLayoutGroupMembers.setError("");
@@ -303,4 +432,109 @@ public class CreatePurchaseActivity extends AppCompatActivity implements View.On
                 && mEditTextCost.getText().length() > 0
                 && participants.size() > 0;
     }
+
+
+    //Test
+    private void selectImage() {
+
+        ActivityCompat.requestPermissions(CreatePurchaseActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                PICK_IMAGE_GALLERY);
+
+        ActivityCompat.requestPermissions(CreatePurchaseActivity.this,
+                new String[]{Manifest.permission.CAMERA},
+                PICK_IMAGE_CAMERA);
+
+        try {
+            PackageManager pm = getPackageManager();
+            int hasPerm = pm.checkPermission(Manifest.permission.CAMERA, getPackageName());
+            if (hasPerm == PackageManager.PERMISSION_GRANTED) {
+                final CharSequence[] options = {"Take Photo", "Choose From Gallery", "Cancel"};
+                android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(CreatePurchaseActivity.this);
+                builder.setTitle("Select Option");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (options[item].equals("Take Photo")) {
+                            dialog.dismiss();
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(intent, PICK_IMAGE_CAMERA);
+                        } else if (options[item].equals("Choose From Gallery")) {
+                            dialog.dismiss();
+                            Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(pickPhoto, PICK_IMAGE_GALLERY);
+                        } else if (options[item].equals("Cancel")) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                builder.show();
+            } else
+                Toast.makeText(this, "Camera Permission error", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Camera Permission error", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        inputStreamImg = null;
+        if (requestCode == PICK_IMAGE_CAMERA) {
+            try {
+                Uri selectedImage = data.getData();
+                bitmap = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
+
+                Log.e("Activity", "Pick from Camera::>>> ");
+
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                destination = new File(Environment.getExternalStorageDirectory() + "/" +
+                        getString(R.string.app_name), "IMG_" + timeStamp + ".jpg");
+                FileOutputStream fo;
+                try {
+                    destination.createNewFile();
+                    fo = new FileOutputStream(destination);
+                    fo.write(bytes.toByteArray());
+                    fo.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                imgPath = destination.getAbsolutePath();
+                imageview.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == PICK_IMAGE_GALLERY) {
+            Uri selectedImage = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bytes);
+                Log.e("Activity", "Pick from Gallery::>>> ");
+
+                imgPath = getRealPathFromURI(selectedImage);
+                destination = new File(imgPath.toString());
+                imageview.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Audio.Media.DATA};
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+    //Fin Test
 }
