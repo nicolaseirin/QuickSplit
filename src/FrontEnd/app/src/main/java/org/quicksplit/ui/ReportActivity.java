ef1eb31e5812a718adbd62368e8079f821f9eccc
Nicolás Eirin <nicoeirin@gmail.com>
2019-06-15 23:49:54 -0300

    Implemented dialog currency spinner

--- src/FrontEnd/app/src/main/java/org/quicksplit/ui/ReportActivity.java
+++ src/FrontEnd/app/src/main/java/org/quicksplit/ui/ReportActivity.java
@@ -1,6 +1,8 @@
 package org.quicksplit.ui;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +13,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.quicksplit.R;
@@ -19,8 +24,10 @@ import org.quicksplit.ServiceGenerator;
 import org.quicksplit.TokenManager;
 import org.quicksplit.adapters.ReportAdapter;
 import org.quicksplit.models.DebtorDebtee;
+import org.quicksplit.service.CurrencyClient;
 import org.quicksplit.service.GroupClient;
 
+import java.util.Currency;
 import java.util.List;
 
 import retrofit2.Call;
@@ -33,22 +40,27 @@ public class ReportActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
 
+    private Spinner mSpinnerCurrency;
+    private ArrayAdapter<String> currenciesArrayAdapter;
+    private List<String> currencies;
+
     private RecyclerView mRecyclerViewReports;
     private ReportAdapter mRecyclerViewReportAdapter;
     private RecyclerView.LayoutManager mRecyclerViewManager;
     private Button mButtonRefresh;
 
-    private int idMenuResource = R.menu.navigation;
+    private int idMenuResource = R.menu.currency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getReports();
+        getCurrencies();
     }
 
     private void buildReportContentView() {
         setContentView(R.layout.activity_report);
-        idMenuResource = R.menu.navigation;
+        idMenuResource = R.menu.currency;
 
         mToolbar = findViewById(R.id.toolbar_top);
         setSupportActionBar(mToolbar);
@@ -61,7 +73,7 @@ public class ReportActivity extends AppCompatActivity {
 
     private void buildNonReportContentView() {
         setContentView(R.layout.activity_non_report);
-        idMenuResource = R.menu.navigation;
+        idMenuResource = R.menu.empty;
 
         mToolbar = findViewById(R.id.toolbar_top);
         setSupportActionBar(mToolbar);
@@ -85,9 +97,15 @@ public class ReportActivity extends AppCompatActivity {
         mButtonRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getReports();
+                getCurrencies();
             }
         });
+    }
+
+    private void buildCurrenciesAdapter() {
+        currenciesArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currencies);
+        currenciesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinnerCurrency.setAdapter(currenciesArrayAdapter);
     }
 
     @Override
@@ -105,6 +123,10 @@ public class ReportActivity extends AppCompatActivity {
                 return true;
             case R.id.refresh:
                 getReports();
+                getCurrencies();
+                return true;
+            case R.id.currency:
+                showCurrenciesDialog();
                 return true;
         }
 
@@ -118,7 +140,7 @@ public class ReportActivity extends AppCompatActivity {
         String groupId = getIntent().getStringExtra("EXTRA_GROUP_ID");
         Call<List<DebtorDebtee>> call = client.getSplitReport(groupId);
 
-        final ProgressDialog loading = ProgressDialog.show(this, "Fetching Data", "Please wait...", false, false);
+        final ProgressDialog loading = ProgressDialog.show(this, getText(R.string.fetching_data), getText(R.string.please_wait), false, false);
 
         call.enqueue(new Callback<List<DebtorDebtee>>() {
             @Override
@@ -144,6 +166,94 @@ public class ReportActivity extends AppCompatActivity {
                 buildErrorReportContentView();
             }
         });
+    }
+
+    private void getReports(String currency) {
+        TokenManager tokenManager = new TokenManager(this);
+        GroupClient client = ServiceGenerator.createService(GroupClient.class, tokenManager.getToken());
+
+        String groupId = getIntent().getStringExtra("EXTRA_GROUP_ID");
+        Call<List<DebtorDebtee>> call = client.getSplitReport(groupId, currency);
+
+        final ProgressDialog loading = ProgressDialog.show(this, getText(R.string.fetching_data), getText(R.string.please_wait), false, false);
+
+        call.enqueue(new Callback<List<DebtorDebtee>>() {
+            @Override
+            public void onResponse(Call<List<DebtorDebtee>> call, Response<List<DebtorDebtee>> response) {
+                if (response.isSuccessful()) {
+                    reports = response.body();
+                    if (reports.size() == 0) {
+                        buildNonReportContentView();
+                    } else {
+                        buildReportContentView();
+                        buildRecyclerViewReport();
+                    }
+                    loading.dismiss();
+                } else {
+                    loading.dismiss();
+                    Toast.makeText(ReportActivity.this, "Error al obtener grupos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<DebtorDebtee>> call, Throwable t) {
+                loading.dismiss();
+                buildErrorReportContentView();
+            }
+        });
+    }
+
+    private void getCurrencies() {
+        TokenManager tokenManager = new TokenManager(this);
+        CurrencyClient client = ServiceGenerator.createService(CurrencyClient.class, tokenManager.getToken());
+        Call<List<String>> call = client.getCurrencies();
+
+        final ProgressDialog loading = ProgressDialog.show(this, getText(R.string.fetching_data), getText(R.string.please_wait), false, false);
+
+        call.enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
+                if (response.isSuccessful()) {
+                    currencies = response.body();
+                    loading.dismiss();
+                } else {
+                    loading.dismiss();
+                    Toast.makeText(ReportActivity.this, "Error al obtener monedas.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<String>> call, Throwable t) {
+                loading.dismiss();
+                buildErrorReportContentView();
+            }
+        });
+    }
+
+    private void showCurrenciesDialog() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(ReportActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_currency, null);
+        mBuilder.setTitle("Moneda");
+        mSpinnerCurrency = mView.findViewById(R.id.spn_currency);
+        buildCurrenciesAdapter();
+        mBuilder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getReports(mSpinnerCurrency.getSelectedItem().toString());
+                dialog.dismiss();
+            }
+        });
+
+        mBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
     }
 
     private void buildRecyclerViewReport() {
