7538fd00a81024a92757e7438f66348bd26bd35d
Nicolás Eirin <nicoeirin@gmail.com>
2019-06-22 20:14:35 -0300

    Finally fixed map

--- src/FrontEnd/app/src/main/java/org/quicksplit/ui/PurchaseMapActivity.java
+++ src/FrontEnd/app/src/main/java/org/quicksplit/ui/PurchaseMapActivity.java
@@ -1,63 +1,58 @@
 package org.quicksplit.ui;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.quicksplit.R;
 
 public class PurchaseMapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
-    protected GoogleApiClient mGoogleApiClient;
     private static final float DEFAULT_ZOOM = 15f;
 
     private GoogleMap mMap;
     private Bundle myBundle;
-    private Double mLongitude;
-    private Double mLatitude;
+    private double mLongitude;
+    private double mLatitude;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        myBundle = this.getIntent().getExtras();
-        mLongitude = myBundle.getDouble("longitude");
-        mLatitude = myBundle.getDouble("latitude");
+        setContentView(R.layout.activity_purchase_map);
+        initMap();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        moveMap();
+        myBundle = this.getIntent().getExtras();
+        mLongitude = myBundle.getDouble("longitude");
+        mLatitude = myBundle.getDouble("latitude");
+        moveCamera((new LatLng(mLatitude, mLongitude)), DEFAULT_ZOOM,"");
     }
 
-    @Override
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
+    private void moveCamera(LatLng latlng, float zoom, String title){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
 
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        if (mMap != null) {
-            moveMap();
+        if(!title.equals("MyLocation")){
+            MarkerOptions options = new MarkerOptions()
+                    .position(latlng)
+                    .title(title);
+            mMap.addMarker(options);
         }
     }
 
-    public void moveMap() {
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLatitude, mLongitude), DEFAULT_ZOOM));
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(PurchaseMapActivity.this);
     }
+
 }
